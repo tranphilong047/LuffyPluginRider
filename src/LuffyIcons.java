3269c3b658761ecdccf452fa57207e962bb59b41
DESKTOP-K3C356F\tranp <longtp@ebk.vn>
2021-11-11 14:12:34 +0700

    initProject

---
+++ src/LuffyIcons.java
@@ -0,0 +1,8 @@
+import com.intellij.openapi.util.IconLoader;
+
+import javax.swing.*;
+
+public interface LuffyIcons {
+    Icon LUFFY_ICON = IconLoader.getIcon("/rsz_luffy.png");
+    Icon RLUFFY_ICON = IconLoader.getIcon("/rsz_rluffy.png");
+}
